ce880e2967da9a691366be5f6b7e2a15db40d1dc
danivek <danielo.kevin@gmail.com>
2015-12-14 16:12:11 +0100

    feat(travis): Add travis.yml fix(gulp-tasks): replace gulp-mocha and gulp-instanbul by gulp-spawn-mocha

--- gulp-tasks/server.js
+++ gulp-tasks/server.js
@@ -3,8 +3,7 @@ var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 var stylish = require('gulp-jscs-stylish');
 var nodemon = require('gulp-nodemon');
-var istanbul = require('gulp-istanbul');
-var mocha = require('gulp-mocha');
+var mocha = require('gulp-spawn-mocha');
 
 gulp.task('server:lint', function() {
   return gulp.src(['server/app.js', 'server/src/**/*.js'])
@@ -51,24 +50,16 @@ gulp.task('server:watch', function() {
   gulp.watch('server/**/*.js', gulp.series(['server:lint-dev']));
 });
 
-gulp.task('server:test', function(cb) {
-  gulp.src(['server/app.js', 'server/src/**/*.js'])
-    .pipe(istanbul()) // Covering files
-    .pipe(istanbul.hookRequire()) // Force `require` to return covered files
-    .on('finish', function() {
-      gulp.src(['test/server/*.js'])
-        .pipe(mocha())
-        .pipe(istanbul.writeReports()) // Creating the reports after tests ran
-        .pipe(istanbul.enforceThresholds({
-          thresholds: {
-            global: 50
-          }
-        })) // Enforce a coverage of at least 50%
-        .on('end', function() {
-          cb();
-          process.exit();
-        });
-    });
+gulp.task('server:test', function() {
+  return gulp.src(['test/server/*.js'], {
+      read: false
+    })
+    .pipe(mocha({
+      R: 'spec',
+      istanbul: {
+        print: 'both'
+      }
+    }));
 });
 
 /* Main Tasks for server */
